7ed0c4756137bba4b0d7ea6a8f6d913af8dc521f
Diego Pucci <diegopucci.me@gmail.com>
2024-06-04 16:01:54 +0300

    Add Antdesign 5

--- superset-frontend/src/components/Badge/index.tsx
+++ superset-frontend/src/components/Badge/index.tsx
@@ -18,8 +18,8 @@
  */
 import React from 'react';
 import { styled } from '@superset-ui/core';
-import { Badge as AntdBadge } from 'antd';
-import { BadgeProps as AntdBadgeProps } from 'antd/lib/badge';
+import { Badge as AntdBadge } from 'antd-v5';
+import { BadgeProps as AntdBadgeProps } from 'antd-v5/lib/badge';
 
 export interface BadgeProps extends AntdBadgeProps {
   textColor?: string;
@@ -29,7 +29,16 @@ const Badge = styled(
   (
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     { textColor, color, text, ...props }: BadgeProps,
-  ) => <AntdBadge text={text} color={text ? color : undefined} {...props} />,
+  ) => (
+    <AntdBadge
+      text={text}
+      color={text ? color : undefined}
+      classNames={{
+        root: 'superset-badge',
+      }}
+      {...props}
+    />
+  ),
 )`
   & > sup {
     padding: 0 ${({ theme }) => theme.gridUnit * 2}px;
